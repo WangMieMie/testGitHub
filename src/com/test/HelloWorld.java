d492f2310e53f2e1807e18784762f44b034e981f
WangMieMie <1025876393@qq.com>
2017-09-18 17:52:54 +0800

    测试提交

---
+++ src/com/test/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.test;
+
+/**
+ * @author :HZY
+ * @version :V1.0
+ * @Package :com.test
+ * @Description :${TODO}
+ * @date :${date} 17:52
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // 测试提交
+    }
+}
